43acf8ec8c63ee87a8dc8afb3c477d9cb814ce41
Imanet4 <imanetaifour2@gmail.com>
2025-08-24 23:08:38 +0100

    fixing typos

--- backend/models/User.js
+++ backend/models/User.js
@@ -12,7 +12,7 @@ const UserSchema = new mongoose.Schema({
 
 
 //Passwod hashing before saving
-userSchema.pre('save', async function (next) {
+UserSchema.pre('save', async function (next) {
     if (!this.isModified('password')) return next();
     this.password = await bcrypt.hash(this.password, 10);
     next(); 
