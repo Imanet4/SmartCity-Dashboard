619b106f5fd48d1ca2bff35f083fabe8ceb320fd
Imanet4 <imanetaifour2@gmail.com>
2025-08-25 10:45:15 +0100

    implementation of the validation to the weather route

--- backend/routes/weather.js
+++ backend/routes/weather.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const weatherController = require('../controllers/weatherController');
 const authController = require('../controllers/authController');
+const { weatherValidation } = require('../utils/validation')
 
 const router = express.Router();
 
-router.get('/:city', weatherController.getCurrentWeather);
-router.get('/history/:city', weatherController.getWeatherHistory);
+router.get('/:city', weatherValidation, weatherController.getCurrentWeather);
+router.get('/history/:city', weatherValidation, weatherController.getWeatherHistory);
 
 //Protected routes (admin only)
 router.use(authController.protect, authController.restrictTo('admin'));
